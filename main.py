7c049f106646f254750f9134721751841a9158bb
lich97zz <136956344@qq.com>
2021-11-23 20:07:18 -0600

    1

--- main.py
+++ main.py
@@ -9,25 +9,10 @@ class Ticket:
             print(attr,"not existed!")
             return
         return self.contents.keys[attr]
-    
+
+#todo configure password and username    
 cmd = "curl https://zccjackhe2.zendesk.com/api/v2/groups.json -v -u yitaohe2@illinois.edu:MissHarry60."
-os.system(cmd)
-
-import sys
-from io import TextIOWrapper, BytesIO
-
-# setup the environment
-old_stdout = sys.stdout
-sys.stdout = TextIOWrapper(BytesIO(), sys.stdout.encoding)
-
-
-# get output
-sys.stdout.seek(0)      # jump to the start
-out = sys.stdout.read() # read output
-
-# restore stdout
-sys.stdout.close()
-sys.stdout = old_stdout
+out = os.popen(cmd).read()
 
 
 print("OUT:",out)
