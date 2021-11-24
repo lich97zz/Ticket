b15bc5414ef58a0d8f7da32be1517baff329a59c
lich97zz <136956344@qq.com>
2021-11-23 20:03:17 -0600

    1

--- main.py
+++ main.py
@@ -10,5 +10,24 @@ class Ticket:
             break
         return self.contents.keys[attr]
     
-cmd = "curl https://zccyitaohe.zendesk.com/api/v2/requests.json -v -u yitaohe2@illinois.edu:Lich97zz."
+cmd = "curl https://zccjackhe2.zendesk.com/api/v2/groups.json -v -u yitaohe2@illinois.edu:MissHarry60."
 os.system(cmd)
+
+import sys
+from io import TextIOWrapper, BytesIO
+
+# setup the environment
+old_stdout = sys.stdout
+sys.stdout = TextIOWrapper(BytesIO(), sys.stdout.encoding)
+
+
+# get output
+sys.stdout.seek(0)      # jump to the start
+out = sys.stdout.read() # read output
+
+# restore stdout
+sys.stdout.close()
+sys.stdout = old_stdout
+
+
+print("OUT:",out)
--- test.py
+++ test.py
@@ -1,3 +1,15 @@
+import json
 content = ""
 with open("test.txt",'r') as f:
     content = f.readlines()
+content = content[0]
+request_str = json.loads(content)
+content_key = content[1:].split(':')[0][1:-1]
+res = []
+for ticket in request_str[content_key]:
+    res.append(ticket)
+
+##balance = 0
+##print(co)
+##for i in range(len(content)):
+##    if content[i]
