630ef0426a137fba0859f50ca8095bb4f99d271b
lich97zz <136956344@qq.com>
2021-11-23 21:13:21 -0600

    1

--- main.py
+++ main.py
@@ -1,29 +1,55 @@
 import os
 from subprocess import PIPE, run
 import json
+import prettytable
+from tabulate import tabulate
+
+
 class Ticket:
     def __init__(self):
         self.contents = dict()
-
+        
+    def __init__(self, content):
+        self.contents = content
+        
     def get_attr(self,attr):
         if attr not in self.contents.keys():
-            print(attr,"not existed!")
-            return
-        return self.contents.keys[attr]
-
+            return " N/A "
+        return self.contents[attr]
+    
+    def simple_info(self):
+        index = self.get_attr('id')
+        subject = self.get_attr('subject')
+        description = self.get_attr('description')
+        return [index,subject,description]
 #todo configure password and username    
 cmd = "curl https://zccjackhe2.zendesk.com/api/v2/requests.json -v -u yitaohe2@illinois.edu:MissHarry60."
 
+def sortf(ticket):
+    return ticket.get_attr('id')
 
 def out(command):
     result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
     return result.stdout
+
+
+def display_ticket_simple(ticket_arr, start_id, end_id):
+    pt = prettytable.PrettyTable(['id','subject','description'])
+    for ticket in ticket_arr[start_id, end_id]:
+        pt.add_row([ticket['id'],ticket['subject'],ticket['description']])
+    print(pt)
 
 content = out(cmd)
 request_str = json.loads(content)
 content_key = content[1:].split(':')[0][1:-1]
 res = []
 for ticket in request_str[content_key]:
-    res.append(ticket)
-    
-print(res[0])
+    res.append(Ticket(ticket))
+res.sort(key=sortf)
+
+display_ticket_simple(res, 0,15)
+print('***\n')
+
+display_ticket_simple(res, 15,30)
+
+
--- test.py
+++ test.py
@@ -1,4 +1,24 @@
 import json
+
+
+class Ticket:
+    def __init__(self):
+        self.contents = dict()
+        
+    def __init__(self, content):
+        self.contents = content
+        
+    def get_attr(self,attr):
+        if attr not in self.contents.keys():
+            return " N/A "
+        return self.contents[attr]
+    
+    def simple_info(self):
+        index = self.get_attr('id')
+        subject = self.get_attr('subject')
+        description = self.get_attr('description')
+        return [index,subject,description]
+    
 content = ""
 with open("test.txt",'r') as f:
     content = f.readlines()
@@ -7,9 +27,13 @@ request_str = json.loads(content)
 content_key = content[1:].split(':')[0][1:-1]
 res = []
 for ticket in request_str[content_key]:
-    res.append(ticket)
-
-##balance = 0
-##print(co)
-##for i in range(len(content)):
-##    if content[i]
+    res.append(Ticket(ticket))
+    
+##import prettytable
+##pt = prettytable.PrettyTable(['id','subject','description'])
+##for ticket in res:
+##    pt.add_row([ticket['id'],ticket['subject'],ticket['description']])
+##print(pt)
+def sortf(ticket):
+    return ticket.get_attr('id')
+res.sort(key=sortf)
